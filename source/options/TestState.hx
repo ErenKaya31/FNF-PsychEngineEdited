8c4fd68bb3db5015b9e22bf567ae410eceb32ef7
Eren Kaya <95748211+ErenKaya31@users.noreply.github.com>
2022-03-14 21:59:24 +0300

    Add files via upload

---
+++ source/options/TestState.hx
@@ -0,0 +1,41 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class TestState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Beta Settings';
+		rpcTitle = 'Beta Settings'; //for Discord Rich Presence
+
+		var option:Option = new Option('In Song Watermarks',
+			"If unchecked in song watermark will be gone",
+			versionTxt.visible = true;
+		addOption(option);
+}
